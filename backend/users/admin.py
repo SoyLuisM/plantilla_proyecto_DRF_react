826e9a8fbc63e3147284061cfe6a050687894863
luis martinez <luismartinezh1200@gmail.com>
2024-12-02 07:36:27 -0600

    personalizando panel de adminstrador

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from users.models import User
+from users.models import User,UserProfile
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'username', 'is_staff', 'is_active', 'is_superuser']
+    fieldsets = (
+        ('datos de usuario', {'fields': ('email','username','password')}),
+        ('Persmisos',  {'fields': ('is_active','is_staff', 'is_superuser','groups', 'user_permissions')})
+    )
+
+@admin.register(UserProfile)
+class UserAdmin(admin.ModelAdmin):
+   list_display = ['nombres', 'a_paterno', 'a_materno', 'is_student']
